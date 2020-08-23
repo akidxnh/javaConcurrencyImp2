404ba7b0c473c3252789b20e9d6f38cd21ae8477
lwp <1284993588@qq.com>
2020-08-23 11:11:10 +0800

    annoations

---
+++ src/main/java/com/lwp/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.lwp.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记线程不安全的类的注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+
+}
